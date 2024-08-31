224294e4948cef97f3ec9e74bdae398e661be351
jbonaiuto <jbonaiuto@gmail.com>
2024-09-01 00:07:08 +1000

    updated for mac support

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ class CustomInstall(install):
         save_path (str): The local path to save the downloaded file.
         """
         if not os.path.exists(save_path):
-            subprocess.check_call(['wget', '-q', '-c', url, '-O', save_path])
+            subprocess.check_call(['curl', '-s', '-L', '-o', save_path, url])
 
 
     def extract_matlab_runtime(self, zip_path, extract_to):
@@ -86,39 +86,79 @@ class CustomInstall(install):
                     if file.endswith('.sh') or file == 'install' or 'bin' in root:
                         os.chmod(os.path.join(root, file), 0o755)
 
-
     def install_matlab_runtime(self):
         """
         Downloads and installs the MATLAB runtime.
+
+        On Linux, it downloads the Linux version of the MATLAB runtime.
+        On macOS, it downloads the macOS version of the MATLAB runtime.
 
         This method downloads the MATLAB runtime ZIP file, extracts it, and runs the installation
         script in silent mode with the necessary parameters.
         """
         base_dir = os.path.abspath(os.path.dirname(__file__))
-        matlab_runtime_zip = os.path.join(base_dir, 'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip')
-        matlab_download_url = (
-            'https://ssd.mathworks.com/supportfiles/downloads/R2019a/Release/9/'
-            'deployment_files/installer/complete/glnxa64/'
-            'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip'
-        )
+        system = sys.platform.system()
+
+        if system == 'Linux':
+            matlab_runtime_zip = os.path.join(base_dir, 'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip')
+            matlab_download_url = (
+                'https://ssd.mathworks.com/supportfiles/downloads/R2019a/Release/9/'
+                'deployment_files/installer/complete/glnxa64/'
+                'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip'
+            )
+        elif system == 'Darwin':  # macOS
+            matlab_runtime_zip = os.path.join(base_dir, 'MATLAB_Runtime_R2019a_Update_9_maci64.dmg.zip')
+            matlab_download_url = (
+                'https://ssd.mathworks.com/supportfiles/downloads/R2019a/Release/9/'
+                'deployment_files/installer/complete/maci64/'
+                'MATLAB_Runtime_R2019a_Update_9_maci64.dmg.zip'
+            )
+        else:
+            raise OSError("Unsupported operating system")
+
+        # Download the appropriate MATLAB runtime file
         self.download_file(matlab_download_url, matlab_runtime_zip)
-        package_dir = self.get_installed_package_dir('spm_standalone')
-        matlab_runtime_extract_dir = os.path.join(package_dir, 'matlab_runtime')
-        self.extract_matlab_runtime(matlab_runtime_zip, matlab_runtime_extract_dir)
 
-        install_script = os.path.join(matlab_runtime_extract_dir, 'install')
-        if not os.path.exists(install_script):
-            raise FileNotFoundError(f"The install script was not found in "
-                                    f"{matlab_runtime_extract_dir}")
+        if system == 'Linux':
+            # Extract and install on Linux
+            package_dir = self.get_installed_package_dir('spm_standalone')
+            matlab_runtime_extract_dir = os.path.join(package_dir, 'matlab_runtime')
+            self.extract_matlab_runtime(matlab_runtime_zip, matlab_runtime_extract_dir)
 
-        destination_dir = os.path.join(package_dir, '../MATLAB_Runtime')
-        subprocess.check_call([
-            install_script, '-mode', 'silent', '-agreeToLicense', 'yes',
-            '-destinationFolder', destination_dir
-        ])
-        shutil.rmtree(matlab_runtime_extract_dir)
-        os.remove(matlab_runtime_zip)
+            install_script = os.path.join(matlab_runtime_extract_dir, 'install')
+            if not os.path.exists(install_script):
+                raise FileNotFoundError(f"The install script was not found in {matlab_runtime_extract_dir}")
 
+            destination_dir = os.path.join(package_dir, '../MATLAB_Runtime')
+            subprocess.check_call([
+                install_script, '-mode', 'silent', '-agreeToLicense', 'yes',
+                '-destinationFolder', destination_dir
+            ])
+
+            # Clean up
+            shutil.rmtree(matlab_runtime_extract_dir)
+        elif system == 'Darwin':
+            # Unzip the dmg.zip file
+            subprocess.check_call(['unzip', '-q', matlab_runtime_zip, '-d', base_dir])
+
+            # Mount the .dmg file
+            dmg_path = os.path.join(base_dir, 'MATLAB_Runtime_R2019a_Update_9_maci64.dmg')
+            mount_point = '/Volumes/MATLAB_Runtime'
+            subprocess.check_call(['hdiutil', 'attach', dmg_path, '-mountpoint', mount_point])
+
+            # Find and run the installer (usually a .pkg file)
+            installer_pkg = os.path.join(mount_point,
+                                         'InstallForMacOSX.app/Contents/Resources/MathWorks/MATLAB_Compiler_Runtime.pkg')
+            if not os.path.exists(installer_pkg):
+                raise FileNotFoundError(f"The installer package was not found in {mount_point}")
+
+            subprocess.check_call(['sudo', 'installer', '-pkg', installer_pkg, '-target', '/'])
+
+            # Unmount the dmg file
+            subprocess.check_call(['hdiutil', 'detach', mount_point])
+
+            # Clean up
+            os.remove(dmg_path)
 
     def set_environment_variables(self):
         """
@@ -134,26 +174,48 @@ class CustomInstall(install):
         activate_script_dir = os.path.join(conda_env_path, "etc", "conda", "activate.d")
         os.makedirs(activate_script_dir, exist_ok=True)
         activate_script_path = os.path.join(activate_script_dir, "env_vars.sh")
-        with open(activate_script_path, "w", encoding="utf-8") as out_file:
-            out_file.write(f'export MATLAB_RUNTIME_DIR="{matlab_runtime_path}"\n')
-            out_file.write('export _OLD_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"\n')
-            out_file.write(
-                'export LD_LIBRARY_PATH="${MATLAB_RUNTIME_DIR}/v96/runtime/glnxa64:'
-                '${MATLAB_RUNTIME_DIR}/v96/bin/glnxa64:'
-                '${MATLAB_RUNTIME_DIR}/v96/sys/os/glnxa64:'
-                '$LD_LIBRARY_PATH"\n'
-            )
-            out_file.write('export XAPPLRESDIR="${MATLAB_RUNTIME_DIR}/v96/X11/app-defaults"\n')
-            out_file.write('')
 
         deactivate_script_dir = os.path.join(conda_env_path, "etc", "conda", "deactivate.d")
         os.makedirs(deactivate_script_dir, exist_ok=True)
         deactivate_script_path = os.path.join(deactivate_script_dir, "env_vars.sh")
-        with open(deactivate_script_path, "w", encoding="utf-8") as out_file:
-            out_file.write('unset MATLAB_RUNTIME_DIR\n')
-            out_file.write('export LD_LIBRARY_PATH="$_OLD_LD_LIBRARY_PATH"\n')
-            out_file.write('unset _OLD_LD_LIBRARY_PATH\n')
-            out_file.write('unset XAPPLRESDIR\n')
+
+        system = sys.platform.system()
+
+        if system == 'Linux':
+            # For Linux
+            with open(activate_script_path, "w", encoding="utf-8") as out_file:
+                out_file.write(f'export MATLAB_RUNTIME_DIR="{matlab_runtime_path}"\n')
+                out_file.write('export _OLD_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"\n')
+                out_file.write(
+                    'export LD_LIBRARY_PATH="${MATLAB_RUNTIME_DIR}/v96/runtime/glnxa64:'
+                    '${MATLAB_RUNTIME_DIR}/v96/bin/glnxa64:'
+                    '${MATLAB_RUNTIME_DIR}/v96/sys/os/glnxa64:'
+                    '$LD_LIBRARY_PATH"\n'
+                )
+                out_file.write('export XAPPLRESDIR="${MATLAB_RUNTIME_DIR}/v96/X11/app-defaults"\n')
+
+            with open(deactivate_script_path, "w", encoding="utf-8") as out_file:
+                out_file.write('unset MATLAB_RUNTIME_DIR\n')
+                out_file.write('export LD_LIBRARY_PATH="$_OLD_LD_LIBRARY_PATH"\n')
+                out_file.write('unset _OLD_LD_LIBRARY_PATH\n')
+                out_file.write('unset XAPPLRESDIR\n')
+
+        elif system == 'Darwin':  # macOS
+            # For macOS
+            with open(activate_script_path, "w", encoding="utf-8") as out_file:
+                out_file.write('export _OLD_DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH"\n')
+                out_file.write(
+                    'export DYLD_LIBRARY_PATH="/Applications/MATLAB/MATLAB_Runtime/v96/runtime/maci64:'
+                    '/Applications/MATLAB/MATLAB_Runtime/v96/sys/os/maci64:'
+                    '/Applications/MATLAB/MATLAB_Runtime/v96/bin/maci64:'
+                    '/Applications/MATLAB/MATLAB_Runtime/v96/extern/bin/maci64:$DYLD_LIBRARY_PATH"\n'
+                )
+
+            with open(deactivate_script_path, "w", encoding="utf-8") as out_file:
+                out_file.write('export DYLD_LIBRARY_PATH="$_OLD_DYLD_LIBRARY_PATH"\n')
+                out_file.write('unset _OLD_DYLD_LIBRARY_PATH\n')
+        else:
+            raise OSError("Unsupported operating system")
 
 
     def get_installed_package_dir(self, package_name):
